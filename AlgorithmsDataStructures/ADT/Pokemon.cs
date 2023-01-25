fc4534fea16d810dca76af3baaf25aaab8a20a88
ABDULGAFFAR ABDULMALIK <abdulgaffarabdulmalik@ABDULGAFFARs-MacBook-Pro.local>
2023-01-25 20:02:46 +0100

    New changes

---
+++ AlgorithmsDataStructures/ADT/Pokemon.cs
@@ -0,0 +1,17 @@
+﻿using System;
+namespace AlgorithmsDataStructures.ADT
+{
+	public class Pokemon
+	{
+
+        public string? Name { get; set; }
+        public int Exp { get; set; }
+
+
+        public bool IsPokemon()
+        {
+            return true;
+        }
+    }
+}
+
--- AlgorithmsDataStructures/Program.cs
+++ AlgorithmsDataStructures/Program.cs
@@ -1,3 +1,20 @@
 ﻿// See https://aka.ms/new-console-template for more information
+using System.Collections;
+
 Console.WriteLine("Hello, World!");
+
+
+int[] array = new int[6];
+
+int length = 0;
+
+for(int x =0; x < 3; x++)
+{
+    array[length] = x;
+    length++;
+    Console.WriteLine(length);
+}
+
+Console.ReadKey();
+
 
